966838d83401faa3f7901e4ae517ad1b5b21a233
Deepakkrishna35 <89923452+Deepakkrishna35@users.noreply.github.com>
2023-03-18 12:36:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -475,9 +475,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     caption=f_caption,
                     protect_content=True if ident == "filep" else False 
                 )
-                await query.answer('Check PM, I have sent files in pm', show_alert=True)
+                await query.answer('Yᴏᴜʀ Fɪʟᴇ Is Rᴇᴀᴅʏ. Tʜᴀɴᴋʏᴏᴜ Fᴏʀ Usɪɴɢ Mᴇ...', show_alert=True)
         except UserIsBlocked:
-            await query.answer('Unblock the bot mahn !', show_alert=True)
+            await query.answer('Dᴜᴅᴇ, Fɪʀsᴛ Uɴʙʟᴏᴄᴋ Mᴇ...', show_alert=True)
         except PeerIdInvalid:
             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
         except Exception as e:
